53190bd91dee1c2303788a13ea752ad6fb4bbad7
Michael Williams <manicdesigner@gmail.com>
2019-05-29 11:14:54 -0400

    feat(HeaderContainer) add import and component HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 function HeaderContainer() {
     return (
         <div>
             <ImageThumbnail />
+            <HeaderTitle />
         </div>
     );
 }
