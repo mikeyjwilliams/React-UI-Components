62d271a1d92faed92f920369945f93bc8abc21bf
Michael Williams <manicdesigner@gmail.com>
2019-05-29 11:28:25 -0400

    feat(HeaderTitle.js) add className to div in HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function HeaderTitle() {
     return (
-        <div>
+        <div className="header-title">
             <h1>Lambda School</h1>
             <h2>@LambdaSchool</h2>
             <p className="date">- 26 jan</p>
