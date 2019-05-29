7828d982cf70bf1c1a4505601fa05f0462cec72a
Michael Williams <manicdesigner@gmail.com>
2019-05-29 11:15:24 -0400

    feat(HeaderTitle) add function HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <h2>@LambdaSchool</h2>
+            <p className="date">- 26 jan</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
