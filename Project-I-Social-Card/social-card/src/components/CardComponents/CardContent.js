8237abb1378c8c3b23e2097c58b0a842b163057a
Michael Williams <manicdesigner@gmail.com>
2019-05-29 13:08:53 -0400

    feat(CardContent.js) + CardContent function component => div children h2, p

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+        <div className="banner-bottom">
+            <h2>Get started with React</h2>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        </div>
+        
+    );
+}
+
+export default CardContent;
