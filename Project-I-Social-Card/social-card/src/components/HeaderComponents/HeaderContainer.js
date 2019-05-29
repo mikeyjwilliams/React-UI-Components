7d9a1dbec048748f514525cb5f0f05ab3dc2062f
Michael Williams <manicdesigner@gmail.com>
2019-05-29 12:13:29 -0400

    feat(HeaderContainer.js) + HeaderCont + import HeaderContent + div wrapping headerTitle & HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 function HeaderContainer() {
     return (
         <div className="header-container">
             <ImageThumbnail />
-            <HeaderTitle />
+            <div>
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
         </div>
     );
 }
