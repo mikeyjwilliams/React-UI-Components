c810cf94db6ae8802ac6e9e3fbd3ca049ca9b3fc
Michael Williams <manicdesigner@gmail.com>
2019-05-29 13:14:05 -0400

    feat(CardContainer.js) + CardContent into span

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 function CardContainer() {
     return (
         <div>
             <span className="border">
                 <CardBanner />
-
+                <CardContent />
             </span>
            
         </div>
