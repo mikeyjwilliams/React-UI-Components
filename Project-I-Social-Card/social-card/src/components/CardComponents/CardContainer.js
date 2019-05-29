7f1e02eb952a10fdf1557b5fe5e12e6b674fd0fc
Michael Williams <manicdesigner@gmail.com>
2019-05-29 13:00:29 -0400

    feat(CardContainer) + CardContainer + holding CardBanner in span

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+
+function CardContainer() {
+    return (
+        <div>
+            <span className="border">
+                <CardBanner />
+
+            </span>
+           
+        </div>
+    );
+}
+
+export default CardContainer;
