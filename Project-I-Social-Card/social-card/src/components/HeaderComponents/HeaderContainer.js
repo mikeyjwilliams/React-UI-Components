1775e8c667cc1917e5368eab5db44dc8c7dc5c2f
Michael Williams <manicdesigner@gmail.com>
2019-05-28 22:34:30 -0400

    feat(HeaderContainer.js) add image to container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+function HeaderContainer() {
+    return (
+        <div>
+            <ImageThumbnail />
+        </div>
+    );
+}
+
+export default HeaderContainer;
