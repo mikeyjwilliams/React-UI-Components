a49dd4630d0c5eaed3f3a0e9de631d9b6d3da00a
Michael Williams <manicdesigner@gmail.com>
2019-05-30 10:24:02 -0400

    doc(FooterContent.js) + comments + format code

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContent.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContent.js
@@ -3,10 +3,10 @@ import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faSync, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-const comment = <FontAwesomeIcon icon={faComment} />
-const sync    = <FontAwesomeIcon icon={faSync} />
-const heart   = <FontAwesomeIcon icon={faHeart} />
-const mail    = <FontAwesomeIcon icon={faEnvelope} />
+const comment = <FontAwesomeIcon icon={faComment} /> // fa-icon icon
+const sync    = <FontAwesomeIcon icon={faSync} /> // fa-sync icon
+const heart   = <FontAwesomeIcon icon={faHeart} /> // fa-heart icon
+const mail    = <FontAwesomeIcon icon={faEnvelope} /> // fa-envelope icon
 
 function FooterContent() {
     return (
@@ -14,10 +14,8 @@ function FooterContent() {
             <span className="fa-size fa-position">{comment}</span>
             <span className="fa-size fa-position">{sync} 6</span> 
             <span className="fa-size fa-position">{heart} 4</span> 
-            <span className="fa-size fa-position">{mail}</span>
-            
+            <span className="fa-size fa-position">{mail}</span>   
         </div>
     );
 }
-
 export default FooterContent;
