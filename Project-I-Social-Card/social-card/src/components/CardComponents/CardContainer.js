a1ad4e4702e2fb57744906c35090f4a38df6c7d3
Michael Williams <manicdesigner@gmail.com>
2019-05-29 22:42:35 -0400

    fix(CardContainer.js) class for component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -5,16 +5,12 @@ import CardContent from './CardContent';
 
 function CardContainer() {
     return (
-        <div>
-            <div className="border">
-                <a href="https://www.reactjs.org">
-                    <CardBanner />
-                    <CardContent />
-                </a>
-            </div>
-           
+        <div className="card">
+            <a href="https://www.reactjs.org">
+                <CardBanner />
+                <CardContent />
+            </a>   
         </div>
     );
 }
-
 export default CardContainer;
