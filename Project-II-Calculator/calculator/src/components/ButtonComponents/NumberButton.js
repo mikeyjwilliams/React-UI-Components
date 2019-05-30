b742661bd495fa0eaab7c7d97534f201b1f14a9f
Michael Williams <manicdesigner@gmail.com>
2019-05-30 18:17:55 -0400

    feat(NumberButton.js) + fontawesome for icons for math signs

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Button.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faDivide, faMinus, faPlus, faEquals } from '@fortawesome/free-solid-svg-icons'
+
+const multiply = <FontAwesomeIcon icon={faTimes} />
+const division = <FontAwesomeIcon icon={faDivide} />
+const subtract = <FontAwesomeIcon icon={faMinus} />
+const addition = <FontAwesomeIcon icon={faPlus} />
+const equals = <FontAwesomeIcon icon={faEquals} />
+
 class NumButton extends React.Component {
     render() {
         return (
