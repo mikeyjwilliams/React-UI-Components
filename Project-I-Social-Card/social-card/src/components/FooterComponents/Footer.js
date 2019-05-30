6c2b270d604eabe1817a05081930fc4869e25541
Michael Williams <manicdesigner@gmail.com>
2019-05-30 10:10:09 -0400

    stretchFeat(Footer.js) holds FooterContent

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer';
+import FooterContent from './FooterContent';
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <FooterContent />
+        </div>
+    );
+}
+
+export default Footer;
