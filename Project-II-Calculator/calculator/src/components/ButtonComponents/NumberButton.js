3435c1f7245989436fb357cc7aec0c5f83712cd3
Michael Williams <manicdesigner@gmail.com>
2019-05-30 11:23:46 -0400

    feat(NumberButton.js) + Button class

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button className="button">
+                {this.props.value}
+            </button>
+        );
+    }
+}
