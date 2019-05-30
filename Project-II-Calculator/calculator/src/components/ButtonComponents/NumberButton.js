7625b07d71f286e8473ef5e3338b16b56f99468f
Michael Williams <manicdesigner@gmail.com>
2019-05-30 12:02:14 -0400

    feat(NumberButton.js) + class CalculatorBtns

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -10,3 +10,29 @@ class Button extends React.Component {
         );
     }
 }
+
+class CalculatorBtns extends React.Component {
+    renderButton(i) {
+        return <Button value={i} />
+    }
+    
+    render() {
+        return (
+            <div class="number-buttons">
+                <div className="board-row">
+                    <span className="one-wide">{this.renderButton(7)}</span>
+                    <span className="one-wide">{this.renderButton(8)}</span>
+                    <span className="one-wide">{this.renderButton(9)}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+function NumberButton() {
+    return (
+        <CalculatorBtns />
+    );
+}
+
+export default NumberButton;
