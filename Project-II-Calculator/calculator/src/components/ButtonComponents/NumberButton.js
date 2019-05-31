db0175146b9408d565e02ab85f5aea3aa3f6f3b3
Michael Williams <manicdesigner@gmail.com>
2019-05-30 21:25:40 -0400

    rm(NumberButton.js) stuff from react doc havent gone over

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -10,46 +10,6 @@ const subtract = <FontAwesomeIcon icon={faMinus} />
 const addition = <FontAwesomeIcon icon={faPlus} />
 const equals = <FontAwesomeIcon icon={faEquals} />
 
-class NumButton extends React.Component {
-    render() {
-        return (
-            <button className="button num-button">
-                {this.props.value}
-            </button>
-        );
-    }
-}
-
-class CalculatorNumBtns extends React.Component {
-    renderButton(i) {
-        return <NumButton value={i} />
-    }
-    
-    render() {
-        return (
-            <div className="number-buttons">
-                <div className="board-row">
-                    <span className="one-wide">{this.renderButton(7)}</span>
-                    <span className="one-wide">{this.renderButton(8)}</span>
-                    <span className="one-wide">{this.renderButton(9)}</span>
-                </div>
-                <div className="board-row">
-                    <span className="one-wide">{this.renderButton(4)}</span>
-                    <span className="one-wide">{this.renderButton(5)}</span>
-                    <span className="one-wide">{this.renderButton(6)}</span>
-                </div>
-                <div className="board-row">
-                    <span className="one-wide">{this.renderButton(1)}</span>
-                    <span className="one-wide">{this.renderButton(2)}</span>
-                    <span className="one-wide">{this.renderButton(3)}</span>
-                </div>
-                <div className="board-row">
-                    <span className="three-wide">{this.renderButton(0)}</span>
-                </div>
-            </div>
-        );
-    }
-}
 
 function NumberButton() {
     return (
