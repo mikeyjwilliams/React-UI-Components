98585022acaaf9aceea4e7d985f575e64e8a526f
Michael Williams <manicdesigner@gmail.com>
2019-05-30 21:26:27 -0400

    rm(ActionButton.js) stuff from doc that havent gone over

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,34 +1,6 @@
 import React from 'react';
 import './Button.css';
 
-class Button extends React.Component {
-    render() {
-        return (
-            <button className="button num-button">
-                {this.props.value}
-            </button>
-        );
-    }
-}
-
-class CalculatorButtons extends React.Component {
-    renderBtn(i) {
-        return <Button value={i} />
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="number-buttons">
-                    <div className="three-wide">{this.renderBtn('clear')}</div>
-                </div>    
-                <div className="calc-signs">
-                    <div className=""></div>
-                </div>
-            </div>
-        );
-    }
-}
 
 function ActionButton() {
     return (
