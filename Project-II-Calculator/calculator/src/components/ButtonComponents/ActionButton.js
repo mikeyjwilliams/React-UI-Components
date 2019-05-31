d90ac7b82bbd81fe9b284b90ca26fbef45da7d36
Michael Williams <manicdesigner@gmail.com>
2019-05-31 11:00:53 -0400

    feat(ActionButton.js) + fontawesome icons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Button.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDivide, faTimes, faMinus, faPlus, faEquals } from '@fortawesome/free-solid-svg-icons'
+
+const division = <FontAwesomeIcon icon={faDivide} />
+const multiply = <FontAwesomeIcon icon={faTimes} />
+const subtract = <FontAwesomeIcon icon={faMinus} />
+const addition = <FontAwesomeIcon icon={faPlus} />
+const equals = <FontAwesomeIcon icon={faEquals} />
 
 function ActionButton() {
     return (
-        <button>+</button>
+        
     );
 }
 
