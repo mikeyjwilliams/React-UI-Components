61dc1e0375e616e3f7a8aeeeabe89ac2b4fecd7c
Michael Williams <manicdesigner@gmail.com>
2019-05-31 11:34:43 -0400

    Clean up actionn button

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import './Button.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDivide, faTimes, faMinus, faPlus, faEquals } from '@fortawesome/free-solid-svg-icons'
 
-const division = <FontAwesomeIcon icon={faDivide} />
-const multiply = <FontAwesomeIcon icon={faTimes} />
-const subtract = <FontAwesomeIcon icon={faMinus} />
-const addition = <FontAwesomeIcon icon={faPlus} />
-const equals = <FontAwesomeIcon icon={faEquals} />
-
-const fontawesome = [
-    {
-        mathSymbol: 'faDivide',
-    },
-    {
-        mathSymbol: 'faTimes'
-    },
-    {
-        mathSymbol: 'faMinus'
-    },
-    {
-        mathSymbol: 'faPlus'
-    },
-    {
-        mathSymbol: 'faEquals'
-    }];
-
-    const fontawesomeArray = fontawesome.map( (symbols) => {
-        return <FontAwesomeIcon icon={symbols} />
-    })
 
     console.log(fontawesomeArray);
 
 function ActionButton(props) {
     return (
-        {props.fontawesome.fontawesomeArray}
+        <button></button>
     );
 }
 
