d1b4143cac91b39b73709ccc231cfa2229d172ce
Michael Williams <manicdesigner@gmail.com>
2019-05-30 21:28:16 -0400

    rm stuff from NumberButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,20 +1,5 @@
 import React from 'react';
 import './Button.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faDivide, faMinus, faPlus, faEquals } from '@fortawesome/free-solid-svg-icons'
-
-const multiply = <FontAwesomeIcon icon={faTimes} />
-const division = <FontAwesomeIcon icon={faDivide} />
-const subtract = <FontAwesomeIcon icon={faMinus} />
-const addition = <FontAwesomeIcon icon={faPlus} />
-const equals = <FontAwesomeIcon icon={faEquals} />
 
 
-function NumberButton() {
-    return (
-        <CalculatorNumBtns />
-    );
-}
-
-export default NumberButton;
