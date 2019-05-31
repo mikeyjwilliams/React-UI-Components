3d1d2b0cd8063f37f1b07d6b189769687b875a86
Michael Williams <manicdesigner@gmail.com>
2019-05-30 23:10:16 -0400

    add key to get rid of error

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Button.css';
 
 
-function NumberButton() {
-
-}
+function NumberButton(props) {
+    return ( 
+        <button className={props.number.class1 +' '+ props.number.class2}>{props.number.num}</button>  
+    );
+};
 export default NumberButton;
