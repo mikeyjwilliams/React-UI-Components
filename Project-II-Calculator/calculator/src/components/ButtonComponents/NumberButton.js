7f043b3968ba6235600d3b5355a248c99ebee619
Michael Williams <manicdesigner@gmail.com>
2019-06-01 17:26:33 -0400

    mod(code) format code

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Button.css';
 
-
 function NumberButton(props) {
     return ( 
         <button className={`numButton ${props.buttonStyle}`}>{props.text}</button>  
