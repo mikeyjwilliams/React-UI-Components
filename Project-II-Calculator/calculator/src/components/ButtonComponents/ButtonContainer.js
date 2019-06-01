dbff0c7f76038c38f76e8bbdcc84c5be42b50d6e
Michael Williams <manicdesigner@gmail.com>
2019-06-01 11:51:14 -0400

    + buttoncontainer + div with section for props.children

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Button.css';
 
 function ButtonContainer(props) {
-
+    <div className={props.buttonWidth}>
+        {props.children}
+    </div>
 }
 export default ButtonContainer;
