940325eba5df027fc9203681c82e69bd72e62eba
Michael Williams <manicdesigner@gmail.com>
2019-06-01 14:44:28 -0400

    mod(CalculatorContainer) + return in statement

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorContainer.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorContainer.js
@@ -3,8 +3,10 @@ import './Display.css';
 import CalculatorDisplay from './CalculatorDisplay';
 
 function CalculatorContainer(props) {
+    return (
     <div className={props.calcDisplay}>
         {props.children}
     </div>
+    ); 
 }
 export default CalculatorContainer;
