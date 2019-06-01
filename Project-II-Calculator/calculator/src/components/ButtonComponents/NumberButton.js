16e7f8b5e6f5d94da5328a0b26afe0b8b5148a3b
Michael Williams <manicdesigner@gmail.com>
2019-06-01 11:50:33 -0400

    alter(NumberButton.js) to just props buttonColor

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -4,7 +4,7 @@ import './Button.css';
 
 function NumberButton(props) {
     return ( 
-        <button className={props.number.class1 +' '+ props.number.class2 +' '+ props.number.class3}>{props.number.num}</button>  
+        <button className={`numButton ${props.buttonColor}`}>{props.data}</button>  
     );
 };
 export default NumberButton;
