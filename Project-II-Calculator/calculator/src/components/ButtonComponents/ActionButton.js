0c63dd2d57c883386651618e2fb4a62a3b78a8b1
Michael Williams <manicdesigner@gmail.com>
2019-06-01 12:13:49 -0400

    mod(ActionButton.js) + classname buttonWidth

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-
-
-    // console.log(fontawesomeArray);
-
 function ActionButton(props) {
     return (
-       <button className={`actionBtn ${props.buttonColor}`}>{props.data}</button>
+       <button className={`actionBtn ${props.buttonWidth} ${props.buttonColor}`}>{props.data}</button>
     );
 }
-
 export default ActionButton;
