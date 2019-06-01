e2c324e813c9564137a90bbca1baafe94e58ee22
Michael Williams <manicdesigner@gmail.com>
2019-06-01 14:29:55 -0400

    add input

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(props) {
+    return (
+       <input type="text" className={props.calcOutput} calc={props.output} />
+    );
+}
+export default CalculatorDisplay;
