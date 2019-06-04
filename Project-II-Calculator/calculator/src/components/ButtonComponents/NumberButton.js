49d461dba9fed316e03ea4382392bf81758ffae4
Michael Williams <manicdesigner@gmail.com>
2019-06-04 10:57:54 -0400

    stretch(NumberButtons) got numbers to console.log to state

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Button.css';
 
-function NumberButton(props) {
-    return ( 
-        <button className={`numButton ${props.buttonStyle}`}>{props.text}</button>  
-    );
-};
+class NumberButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: ""
+        }
+    }
+    displayNumber = (event) => {
+        this.setState({num: [this.state.num, this.props.text]})
+        console.log(this.state.num);
+    }
+    render() {
+        return ( 
+            <button type="button" className={`numButton ${this.props.buttonStyle}`}
+            onClick={this.displayNumber}>{this.props.text} </button>  
+        );
+    }
+
+}
 export default NumberButton;
